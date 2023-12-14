2a53e32e7dd03729c7707d2248664081a95328e3
Hubert Dzikuc <YOUR_EMAIL>
2023-12-14 17:37:07 +0100

    OneOf.As

---
+++ OneOf.Else/OneOf.Else/OneOfAsExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace OneOf.Else;
+
+public static class OneOfAsExtensions
+{
+    public static OneOf<T0, T1, T2> As<T0, T1, T2>(this OneOf<T0, T1> oneOf)
+    {
+        return oneOf.Match<OneOf<T0, T1, T2>>(t0 => t0, t1 => t1);
+    }
+}
